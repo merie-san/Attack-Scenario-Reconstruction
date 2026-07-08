completed testing and implementation of metrics calculation bugs + minor bug fixes

scenario_reconstructor/reconstruct_cAPTure.py
@@ -86,9 +86,12 @@ BRUTE_FORCE_MALFORMED = AttackType(BRUTE_FORCE_MALFORMED_NAME,
                                     Preconditions({DollarCharHostAttributes.SSH_DISCOVERED}, False)},
                                    {DollarCharHostAttributes.SSH_COMPROMISED})
 DOLLAR_CHAR = AttackType(DOLLAR_CHAR_NAME, {Preconditions({DollarCharHostAttributes.MQTT_MACHINE}, True),
-                                            Preconditions({DollarCharHostAttributes.SSH_COMPROMISED}, True),
-                                            Preconditions({DollarCharHostAttributes.SCRIPTS_INSTALLED}, True),
-                                            Preconditions({DollarCharHostAttributes.MQTT_DISCOVERED}, False),
+                                            Preconditions(
+                                                {DollarCharHostAttributes.SSH_COMPROMISED}, True),
+                                            Preconditions(
+                                                {DollarCharHostAttributes.SCRIPTS_INSTALLED}, True),
+                                            Preconditions(
+                                                {DollarCharHostAttributes.MQTT_DISCOVERED}, False),
                                             Preconditions({DollarCharHostAttributes.BROKER_MACHINE}, False)},
                          {DollarCharHostAttributes.CRASHED})
 SCP_INST = AttackType(SCP_INST_NAME, {Preconditions({DollarCharHostAttributes.START_MACHINE}, True),
@@ -96,10 +99,11 @@ SCP_INST = AttackType(SCP_INST_NAME, {Preconditions({DollarCharHostAttributes.ST
                       {DollarCharHostAttributes.SCRIPTS_INSTALLED})
 UNDETERMINED = AttackType("undetermined", {Preconditions(
     {DollarCharHostAttributes.START_MACHINE, DollarCharHostAttributes.SSH_COMPROMISED}, True)},
-                          {DollarCharHostAttributes.SSH_DISCOVERED, DollarCharHostAttributes.MQTT_DISCOVERED,
-                           DollarCharHostAttributes.SSH_COMPROMISED, DollarCharHostAttributes.CRASHED,
-                           DollarCharHostAttributes.SCRIPTS_INSTALLED})
-ATK_TYPES = [NMAP_MQTT, NMAP_10_T5, NMAP_BANNER, BRUTE_FORCE_MALFORMED, DOLLAR_CHAR, SCP_INST]
+    {DollarCharHostAttributes.SSH_DISCOVERED, DollarCharHostAttributes.MQTT_DISCOVERED,
+     DollarCharHostAttributes.SSH_COMPROMISED, DollarCharHostAttributes.CRASHED,
+     DollarCharHostAttributes.SCRIPTS_INSTALLED})
+ATK_TYPES = [NMAP_MQTT, NMAP_10_T5, NMAP_BANNER,
+             BRUTE_FORCE_MALFORMED, DOLLAR_CHAR, SCP_INST]
 
 HOSTS = [Host(ip) for ip in IP_LIST if ip != "10.0.0.1" and ip != "10.0.0.2"]
 for host in HOSTS:
@@ -113,8 +117,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="reconstructScenario",
                                      description="Reconstruct scenarios and compute metrics from the cAPTure dataset, with provided macros defined in the same python file")
     parser.add_argument("-f", "--file", required=True, help="file name")
-    parser.add_argument("-t", "--times", help="number of evaluation cycles", default=100, type=int)
-    parser.add_argument("-l", "--log", help="where to log results", default="./log.txt", type=str)
+    parser.add_argument(
+        "-t", "--times", help="number of evaluation cycles", default=100, type=int)
+    parser.add_argument(
+        "-l", "--log", help="where to log results", default="./log.txt", type=str)
     parser.add_argument("--anomaly-threshold", help="threshold for alert generation", default=0.8,
                         type=float)
     parser.add_argument("--suspect-threshold", help="threshold for suspicion addition", default=0.4,
@@ -158,48 +164,58 @@ if __name__ == '__main__':
     scenario_soundness_list = []
     scenario_completeness_list = []
 
+    print("starting to run the iterations...")
+
+    for i in range(args.times):
+        print("generating new scenario...")
+        scenario_df = generator.generate_scenario(10)
+        steps, alerts = generator.export_results(ATK_TYPES)
+        print("actual exploits:")
+        print("\n".join([str(step) for step in steps]) + "")
+        print("actual alerts:")
+        print("\n".join([str(alert) for alert in alerts[-20:]]) + "")
+        print("converting scenario in flow objects...")
+        flows = flow_convertor.convert(scenario_df)
+        print("reconstructing scenario from flow objects...")
+        for flow in flows:
+            manager.accept(flow)
+        p_steps, p_alerts = manager.get_results()
+        print("predicted exploits:")
+        print("\n".join([str(step) for step in p_steps]))
+        print("predicted alerts:")
+        print("\n".join([str(alert) for alert in p_alerts[-20:]]))
+        print("predicted false negatives")
+        print("\n".join([str(fn) for fn in manager.get_fns()]))
+        print("predicted false positives")
+        print("\n".join([str(fp) for fp in manager.get_fps()]))
+        manager.reset(initial_attributes)
+        print("calculating metrics...")
+        tn, fp, fn, tp = metrics_calc.get_detection_confusion_matrix_capture(
+            scenario_df, args.anomaly_threshold)
+        tn_r, fp_r, fn_r, tp_r = metrics_calc.get_reconstruction_confusion_matrix_capture(
+            scenario_df, p_alerts)
+        delta_tn_list.append(tn_r - tn)
+        delta_fp_list.append(fp_r - fp)
+        delta_fn_list.append(fn_r - fn)
+        delta_tp_list.append(tp_r - tp)
+        step_soundness, step_completeness = metrics_calc.get_step_soundness_completeness(
+            p_steps, steps, p_alerts, alerts, UNDETERMINED)
+        step_soundness_list.append(step_soundness)
+        step_completeness_list.append(step_completeness)
+        scenario_recall_list.append(metrics_calc.get_scenario_step_recall(
+            p_steps, steps, args.torelance, UNDETERMINED))
+        scenario_precision_list.append(metrics_calc.get_scenario_step_precision(
+            p_steps, steps, args.torelance, UNDETERMINED))
+        scenario_recall_nt_list.append(
+            metrics_calc.get_scenario_step_recall_no_timing(p_steps, steps, UNDETERMINED))
+        scenario_precision_nt_list.append(
+            metrics_calc.get_scenario_step_precision_no_timing(p_steps, steps, UNDETERMINED))
+        scenario_soundness, scenario_completeness = metrics_calc.get_scenario_soundness_completeness(
+            p_steps, steps, UNDETERMINED)
+        scenario_soundness_list.append(scenario_soundness)
+        scenario_completeness_list.append(scenario_completeness)
+
     with open(args.log, "w") as f:
-
-        print("starting to run the iterations...")
-
-        for i in range(args.times):
-            print("generating new scenario...")
-            scenario_df = generator.generate_scenario(10)
-            steps, alerts = generator.export_results(ATK_TYPES)
-            print("actual exploits:")
-            print("\n".join([str(step) for step in steps]) + "")
-            print("actual alerts:")
-            print("\n".join([str(alert) for alert in alerts[-20:]]) + "")
-            print("converting scenario in flow objects...")
-            flows = flow_convertor.convert(scenario_df)
-            print("reconstructing scenario from flow objects...")
-            for flow in flows:
-                manager.accept(flow)
-            p_steps, p_alerts = manager.get_results()
-            print("predicted exploits:")
-            print("\n".join([str(step) for step in p_steps]))
-            print("predicted alerts:")
-            print("\n".join([str(alert) for alert in p_alerts[-20:]]))
-            print("predicted false negatives")
-            print("\n".join([str(fn) for fn in manager.get_fns()]))
-            print("predicted false positives")
-            print("\n".join([str(fp) for fp in manager.get_fps()]))
-            manager.reset(initial_attributes)
-            print("calculating metrics...")
-            tn, fp, fn, tp = metrics_calc.get_detection_confusion_matrix_capture(scenario_df, args.anomaly_threshold)
-            tn_r, fp_r, fn_r, tp_r = metrics_calc.get_reconstruction_confusion_matrix_capture(scenario_df, p_alerts)
-            delta_tn_list.append(tn_r - tn)
-            delta_fp_list.append(fp_r - fp)
-            delta_fn_list.append(fn_r - fn)
-            delta_tp_list.append(tp_r - tp)
-            step_soundness_list.append(metrics_calc.get_step_soundness(p_steps, steps, p_alerts, alerts))
-            step_completeness_list.append(metrics_calc.get_step_completeness(p_steps, steps, p_alerts, alerts))
-            scenario_recall_list.append(metrics_calc.get_scenario_step_recall(p_steps, steps, args.torelance))
-            scenario_precision_list.append(metrics_calc.get_scenario_step_precision(p_steps, steps, args.torelance))
-            scenario_recall_nt_list.append(metrics_calc.get_scenario_step_recall_no_timing(p_steps, steps))
-            scenario_precision_nt_list.append(metrics_calc.get_scenario_step_precision_no_timing(p_steps, steps))
-            scenario_soundness_list.append(metrics_calc.get_scenario_soundness(p_steps, steps))
-            scenario_completeness_list.append(metrics_calc.get_scenario_completeness(p_steps, steps))
 
         f.write(
             f"mean true negative deviation of reconstruction results from initial intrusion detection results: {float(np.mean(delta_tn_list))}\n")
@@ -210,12 +226,19 @@ if __name__ == '__main__':
         f.write(
             f"mean true positives deviation of reconstruction results from initial intrusion detection results: {float(np.mean(delta_tp_list))}\n")
 
-        f.write(f"mean soundness of reconstructed steps: {float(np.mean(step_soundness_list))}\n")
-        f.write(f"mean completeness of reconstructed steps: {float(np.mean(step_completeness_list))}\n")
-        f.write(f"mean precision of reconstructed scenarios: {float(np.mean(scenario_precision_list))}\n")
-        f.write(f"mean recall of reconstructed scenarios: {float(np.mean(scenario_recall_list))}\n")
+        f.write(
+            f"mean soundness of reconstructed steps: {float(np.mean(step_soundness_list))}\n")
+        f.write(
+            f"mean completeness of reconstructed steps: {float(np.mean(step_completeness_list))}\n")
+        f.write(
+            f"mean precision of reconstructed scenarios: {float(np.mean(scenario_precision_list))}\n")
+        f.write(
+            f"mean recall of reconstructed scenarios: {float(np.mean(scenario_recall_list))}\n")
         f.write(
             f"mean precision of reconstructed scenarios (no timing): {float(np.mean(scenario_precision_nt_list))}\n")
-        f.write(f"mean recall of reconstructed scenarios (no timing): {float(np.mean(scenario_recall_nt_list))}\n")
-        f.write(f"mean soundness of reconstructed scenarios: {float(np.mean(scenario_soundness_list))}\n")
-        f.write(f"mean completeness of reconstructed scenarios: {float(np.mean(scenario_completeness_list))}\n")
+        f.write(
+            f"mean recall of reconstructed scenarios (no timing): {float(np.mean(scenario_recall_nt_list))}\n")
+        f.write(
+            f"mean soundness of reconstructed scenarios: {float(np.mean(scenario_soundness_list))}\n")
+        f.write(
+            f"mean completeness of reconstructed scenarios: {float(np.mean(scenario_completeness_list))}\n")